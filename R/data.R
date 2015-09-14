561d134b3b75c84c6e801b56f7d688c361b8fd62
afukushima <atsushi.fukushima@riken.jp>
2015-09-14 16:07:45 +0900

    SummarizedExperiment of AtMeKO dataset

---
+++ R/data.R
@@ -0,0 +1,184 @@
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' 656 samples (6-8 biological replicates for each mutant) including
+##' 629 plants and 27 quality control samples. Please note that
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' To compare the metabolic phenotypes of the 50 mutants simultaneously,
+##' we constructed a single data matrix consisting of the five data sets
+##' (designated the 'ALL' data set) with appropriate preprocessing and
+##' normalizing methods.
+##' 
+##' A matrix containing  242 detected metabolites (rows) and 656 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_ALL
+##' @docType data
+##' @aliases AtMeKO_ALL
+##' @title A metabolite data set from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' This is subset of our project (MeKO). See also \link{AtMEKO_ALL data}. Please note that
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' Of these 5 batch datasets, this data corresponds to "Batch 01".
+##' 
+##' A matrix containing 228 detected metabolites (rows) and 116 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_Batch01
+##' @docType data
+##' @aliases AtMeKO_B01
+##' @title A metabolite sub-dataset (called Batch01) from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' This is subset of our project (MeKO). See also \link{AtMEKO_ALL data}. Please note that
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' Of these 5 batch datasets, this data corresponds to "Batch 02".
+##' 
+##' A matrix containing  202 detected metabolites (rows) and 144 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_Batch02
+##' @docType data
+##' @aliases AtMeKO_B02
+##' @title A metabolite sub-dataset (called Batch02) from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' This is subset of our project (MeKO). See also \link{AtMEKO_ALL data}. Please note that
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' Of these 5 batch datasets, this data corresponds to "Batch 03".
+##' 
+##' A matrix containing 217  detected metabolites (rows) and 120 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_Batch03
+##' @docType data
+##' @aliases AtMeKO_B03
+##' @title A metabolite sub-dataset (called Batch03) from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' This is subset of our project (MeKO). See also \link{AtMEKO_ALL data}. Please note that 
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' Of these 5 batch datasets, this data corresponds to "Batch 04".
+##' 
+##' A matrix containing 233 detected metabolites (rows) and 135 observations 
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_Batch04
+##' @docType data
+##' @aliases AtMeKO_B04
+##' @title A metabolite sub-dataset (called Batch04) from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial
+##' parts of 50 mutants and the wild type plants (Col-0) were analyzed
+##' by GC-TOF/MS. 
+##'
+##' This is subset of our project (MeKO). See also \link{AtMEKO_ALL data}. Please note that
+##' we analyzed them on 5 different days and preprocessed them separately.
+##' Of these 5 batch datasets, this data corresponds to "Batch 05".
+##' 
+##' A matrix containing 239 detected metabolites (rows) and 104 observations 
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS47
+##' 
+##' All raw data in netCDF are also available from DropMet
+##' (http://prime.psc.riken.jp/?action=drop_index),
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS47)
+##' and The MetabolomeExpress (Carroll et al., 2010).
+##' Users can also see our data in the PMR (Hur et al., 2013).
+##' 
+##' @name AtMeKO_Batch05
+##' @docType data
+##' @aliases AtMeKO_B05
+##' @title A metabolite sub-dataset (called Batch05) from 50 Arabidopsis mutants by GC-TOF/MS
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. Plant Physiol 2014 165:948-961.
+##' @author Atsushi Fukushima
+NULL
+
+
