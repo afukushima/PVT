be143e102bb460f125480557417744ed69c5025b
afukushima <atsushi.fukushima@riken.jp>
2015-09-16 16:43:47 +0900

    added new GCMS metabolome datasets

--- R/data.R
+++ R/data.R
@@ -246,3 +246,100 @@ NULL
 ##' @author Atsushi Fukushima
 NULL
 
+
+##' A metabolite data set. The Arabidopsis metabolome of the roots of
+##' 3 genotypes (Col-0, mto1, and tt4) were analyzed by GC-TOF/MS.
+##' 
+##'
+##' 53 samples (17 biological replicates for Col-0, 20 for tt4, and 16 for mto1)
+##' 
+##' A matrix containing  166 annotated metabolites (rows) and 53 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS45
+##' 
+##' All raw data in netCDF are also available from 
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS45).
+##' 
+##' @name AtMet_Fukushima11
+##' @docType data
+##' @aliases AtMet_F11
+##' @title The Arabidopsis metabolome of the roots of 3 genotypes (Col-0, mto1, and tt4) were analyzed by GC-TOF/MS.
+##' 
+##' @keywords datasets
+##' @references Atsushi Fukushima et al. BMC Syst Biol 2011 5:1.
+##' @author Atsushi Fukushima
+NULL
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial parts of
+##' 3 genotypes (Col-0, mto1, and tt4) were analyzed by GC-TOF/MS.
+##' 
+##'
+##' 50 samples (17 biological replicates for Col-0, 20 for tt4, and 13 for mto1)
+##' 
+##' A matrix containing  171 annotated metabolites (rows) and 50 observations
+##' (columns).
+##'
+##' MetaboLights accession no.: MTBLS40
+##' 
+##' All raw data in netCDF are also available from 
+##' MetaboLights (Salek et al., 2013; accession no. MTBLS40).
+##' 
+##' @name AtMet_Kusano07
+##' @docType data
+##' @aliases AtMet_K07
+##' @title The Arabidopsis metabolome of the aerial parts of 3 genotypes (Col-0, mto1, and tt4) were analyzed by GC-TOF/MS.
+##' 
+##' @keywords datasets
+##' @references Miyako Kusano et al. BMC Syst Biol 2007 1:53.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial parts of
+##' wild-type plant (Col-0) and the three mto mutants (mto1, mto2, and mto3)
+##' were analyzed by GC-TOF/MS.
+##' 
+##'
+##' 58 samples (including 4 biological replicates for mto1-1,
+##' 6 for mto1-4, 8 for mto2-1, 5 for mto3-1 and mto3-2, and
+##' the rest for Col-0)
+##' 
+##' A matrix containing 213 annotated metabolites (rows) and 58 observations
+##' (columns).
+##'
+##' @name AtMet_Kusano10
+##' @docType data
+##' @aliases AtMet_K10
+##' @title The Arabidopsis metabolome of the aerial parts of 3 mto mutants (mto1, mto2, and mto3) were analyzed by GC-TOF/MS.
+##' 
+##' @keywords datasets
+##' @references Miyako Kusano et al. Amino Acids 2010 39:1013-1021.
+##' @author Atsushi Fukushima
+NULL
+
+
+##' A metabolite data set. The Arabidopsis metabolome of the aerial parts of
+##' wild-type plant (Col-0) and the 3 mutants (tt4, tt5, and sng1)
+##' were analyzed by GC-TOF/MS. We investigated metabolic responses of
+##' Arabidopsis to UV-B light.
+##' 
+##' We used mutants impaired in flavonoid (transparent testa 4, tt4;
+##' transparent testa 5, tt5) or sinapoyl-malate (sinapoylglucose
+##' accumulator 1, sng1) biosynthesis,  exposed to a short 24-h or
+##' a longer 96-h exposure to this photo-oxidative stress.
+##' 
+##' A matrix containing 213 annotated metabolites (rows) and 58 observations
+##' (columns).
+##'
+##' @name AtMet_Kusano11
+##' @docType data
+##' @aliases AtMet_K11
+##' @title The Arabidopsis metabolome  3 mutants (tt4, tt5, and sng1) under UV light were analyzed by GC-TOF/MS.
+##' 
+##' @keywords datasets
+##' @references Miyako Kusano et al. Plant J 2011 67:354-369.
+##' @author Atsushi Fukushima
+NULL
+
+
